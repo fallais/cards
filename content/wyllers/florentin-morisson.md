a31f267099e369f03e1f342e6e99d78fc3295566
root <francois.allais@hotmail.com>
2023-12-04 15:01:25 +0100

    working

--- content/wyllers/florentin-morisson.md
+++ content/wyllers/florentin-morisson.md
@@ -3,7 +3,7 @@ title: "Florentin MORISSON"
 job: "Responsable d'agence"
 photo: "fmorisson.jpg"
 date: 2023-08-01
-phone: 06 01 01 01 01
+phone: 0767877288
 email: f.morisson@wyll.io
 linkedin: https://www.linkedin.com/in/%E2%98%81%EF%B8%8F-florentin-morisson-829b75148
 tags: ["recrutement", "commerce", "management"]
--- static/main.js
+++ static/main.js
@@ -1,11 +1,14 @@
-new QRCode(document.getElementById("qrcode"),{ 
-    text: "https://www.nextcard.fr/vcard/rhp56chb",
-    width: 110,
-    height: 110,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
+$('document').ready(function(){
+    new QRCode(document.getElementById("qrcode"),{ 
+        text: "https://www.nextcard.fr/vcard/rhp56chb",
+        width: 110,
+        height: 110,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
 });
+
 
 $.notify.defaults({
   autoHide: true
