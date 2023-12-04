874e14b1890853e0a235854722a45e345538814f
root <francois.allais@hotmail.com>
2023-12-04 10:14:02 +0100

    wip

---
+++ static/main.js
@@ -0,0 +1,22 @@
+new QRCode(document.getElementById("qrcode"),{ 
+    text: "https://www.nextcard.fr/vcard/rhp56chb",
+    width: 110,
+    height: 110,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
+
+$.notify.defaults({
+  autoHide: true
+});
+
+function copyToClipboard(value){
+    console.log(value)
+    navigator.clipboard.writeText(value).then(function() {
+        $(".contact").notify("Copié !", { position:"top center", className:"success" });
+        console.log(value, "has been copied");
+    }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
